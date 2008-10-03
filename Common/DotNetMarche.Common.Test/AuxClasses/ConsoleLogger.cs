90404486d6770db8b583e7948e4087217199c3ac
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2008-10-03 09:08:27 +0000

    Added some basic tests on GlobalTransactionManager and corrected some bugs.

---
+++ Common/DotNetMarche.Common.Test/AuxClasses/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetMarche.Infrastructure.Logging;
+
+namespace DotNetMarche.Common.Test.AuxClasses
+{
+	public class ConsoleLogger : ILogger
+	{
+		#region ILogger Members
+
+		public LogLevel ActualLevel
+		{
+			get { return LogLevel.Verbose; }
+		}
+
+		public void Log(LogLevel level, string message, Exception ex)
+		{
+			if (level < LogLevel.Error)
+				Console.WriteLine("{0}:\t{1} {2}", level, message, ex);
+			else
+				Console.Error.WriteLine("{0}:\t{1} {2}", level, message, ex);
+		}
+
+		#endregion
+	}
+}
