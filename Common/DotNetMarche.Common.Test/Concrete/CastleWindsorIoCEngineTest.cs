c71359435dc72ae37cc1d8f7ca8c680a522a0cad
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2008-10-06 16:33:27 +0000

    Added tests for two database transaction.

---
+++ Common/DotNetMarche.Common.Test/Concrete/CastleWindsorIoCEngineTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetMarche.Common.Test.Concrete.Classes;
+using DotNetMarche.Infrastructure.Concrete;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace DotNetMarche.Common.Test.Concrete
+{
+	[TestFixture]
+	public class CastleWindsorIoCEngineTest
+	{
+
+		private CastleWindsorIoCEngine CastleTest1;
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			CastleTest1 = new CastleWindsorIoCEngine("Concrete\\files\\CastleTest1.xml");
+		}
+
+		#region Tests with configuration file 1
+
+		[Test]
+		public void TestResolveBase()
+		{
+			IIoCBase obj = CastleTest1.Resolve<IIoCBase>();
+			Assert.That(obj, Is.Not.Null);
+			Assert.That(obj.Value, Is.EqualTo(10));
+		}
+
+		[Test]
+		public void TestResolveWithName()
+		{
+			IIoCBase obj = CastleTest1.Resolve<IIoCBase>("BaseNamed");
+			Assert.That(obj, Is.Not.Null);
+			Assert.That(obj.Value, Is.EqualTo(12));
+		}		
+		
+		[Test, ExpectedException()]
+		public void TestResolveComponentWithUncompleteDependance()
+		{
+			IIoCChain obj = CastleTest1.Resolve<IIoCChain>();
+		}
+
+		[Test]
+		public void TestResolveBaseWithExplicitDependance()
+		{
+			IIoCBase obj = CastleTest1.ResolveWithName<IIoCBase>("BaseNoParam", "needValue", 99);
+			Assert.That(obj.Value, Is.EqualTo(99));
+		}
+
+		[Test]
+		public void TestResolveChainWithExplicitDependance()
+		{
+			IIoCChain obj = CastleTest1.Resolve<IIoCChain>("needValue", 98);
+			Assert.That(obj.TheBase.Value, Is.EqualTo(98));
+		}
+
+		[Test]
+		public void TestResolveChainWithExplicitDependance2()
+		{
+			IIoCChain obj = CastleTest1.ResolveWithName<IIoCChain>("ChainDependant2", "needValue", 97);
+			Assert.That(obj.TheBase.Value, Is.EqualTo(97));
+		}
+
+		#endregion
+	}
+}
--- Common/DotNetMarche.Infrastructure.Concrete/CastleWindsorIoCEngine.cs
+++ Common/DotNetMarche.Infrastructure.Concrete/CastleWindsorIoCEngine.cs
@@ -29,6 +29,48 @@ namespace DotNetMarche.Infrastructure.Concrete
 			return container.Resolve<T>();
 		}
 
+		public T Resolve<T>(String objectName)
+		{
+			return container.Resolve<T>(objectName);
+		}
+
+		/// <summary>
+		/// This is the version used when I want to specify from external 
+		/// caller some of the dependencies. This is expecially useful for testing
+		/// purpose.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="values"></param>
+		/// <returns></returns>
+		public T Resolve<T>(params object[] values)
+		{
+			System.Collections.Hashtable arguments = new System.Collections.Hashtable();
+			for (Int32 I = 0; I < values.Length; I += 2)
+			{
+				arguments.Add(values[I], values[I + 1]);
+			}
+			return container.Resolve<T>(arguments);
+		}
+
+		/// <summary>
+		/// This is the version used when I want to specify from external 
+		/// caller some of the dependencies. This is expecially useful for testing
+		/// purpose.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="values"></param>
+		/// <param name="name">Name of the object to retrieve</param>
+		/// <returns></returns>
+		public T ResolveWithName<T>(String name, params object[] values)
+		{
+			System.Collections.Hashtable arguments = new System.Collections.Hashtable();
+			for (Int32 I = 0; I < values.Length; I += 2)
+			{
+				arguments.Add(values[I], values[I + 1]);
+			}
+			return container.Resolve<T>(name, arguments);
+		}
+
 		#endregion
 	}
 }
