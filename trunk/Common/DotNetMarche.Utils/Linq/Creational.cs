aa86b9d799a87f9766fada051c15269f76d77492
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2008-11-20 17:14:48 +0000

    First tentative to create a  test for multithreading routines

---
+++ trunk/Common/DotNetMarche.Utils/Linq/Creational.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetMarche.Utils.Linq
+{
+	public static class Creational
+	{
+		public static T[] Fill<T>(this T[] original, Func<T> creator)
+		{
+			for (Int32 I = 0; I < original.Length; ++I)
+				original[I] = creator();
+			return original;
+		}
+	}
+}
