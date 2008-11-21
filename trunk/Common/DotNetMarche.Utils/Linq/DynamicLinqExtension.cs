b99dc9b99647818e10ce6c017d66467d59ea4b98
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2008-11-21 18:19:06 +0000

    Little correction still need to manage conversion of parameters.

--- trunk/Common/DotNetMarche.Utils/Linq/DynamicLinqExtension.cs
+++ trunk/Common/DotNetMarche.Utils/Linq/DynamicLinqExtension.cs
@@ -20,5 +20,20 @@ namespace DotNetMarche.Utils.Linq
 			Func<T, Boolean> predicate = DynamicLinq.ParseToFunction<T, Boolean>(query);
 			return source.Where(predicate);
 		}
+
+		/// <summary>
+		/// <see cref="Where{T}(IEnumerable{T},string)"/>.
+		/// This version support parameters.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="query"></param>
+		/// <param name="param1"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> Where<T>(this IEnumerable<T> source, String query, Object param1)
+		{
+			Func<T, Object, Boolean> predicate = DynamicLinq.ParseToFunction<T, Object, Boolean>(query);
+			return source.Where(e => predicate(e, param1));  //Currying 
+		}
 	}
 }
