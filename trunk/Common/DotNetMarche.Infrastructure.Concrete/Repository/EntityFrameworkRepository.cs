b8f13e230cb9d4f7e696158d5824b2692f9a38eb
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2008-11-26 16:49:42 +0000

    Emtpy entity framework repository

---
+++ trunk/Common/DotNetMarche.Infrastructure.Concrete/Repository/EntityFrameworkRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetMarche.Infrastructure.Data;
+
+namespace DotNetMarche.Infrastructure.Concrete.Repository
+{
+	public class EntityFrameworkRepository<T> : IRepository<T>
+	{
+		#region IRepository<T> Members
+
+		public T GetById(object id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Save(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SaveOrUpdate(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<T> Query()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
