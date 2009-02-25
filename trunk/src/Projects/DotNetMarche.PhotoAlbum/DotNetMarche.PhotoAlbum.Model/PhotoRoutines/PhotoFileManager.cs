e14aee1cd4ddbe2c5c2d4274fd72be062bd098e6
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2009-02-25 19:26:53 +0000

    Update the base structure of PhotoAlbum

---
+++ trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Model/PhotoRoutines/PhotoFileManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DotNetMarche.PhotoAlbum.Model.PhotoRoutines
+{
+   /// <summary>
+   /// Generates names from the photo.
+   /// </summary>
+   public static class PhotoFileManager
+   {
+
+      public static Func<String> NameRandomGenerator = () =>
+       {
+          RNGCryptoServiceProvider provider =
+             new RNGCryptoServiceProvider();
+          Byte[] name = new byte[32];
+          provider.GetBytes(name);
+          return BitConverter.ToString(name).Replace("-", "");
+       };
+
+      public static DisposableAction OverrideGenerator(Func<String> newGenerator)
+      {
+         Func<String> current = NameRandomGenerator;
+         NameRandomGenerator = newGenerator;
+         return new DisposableAction(() => NameRandomGenerator = current  );
+      }
+
+      public static string GenerateName()
+      {
+         String randomFileName = null;
+         while (String.IsNullOrEmpty(randomFileName) || File.Exists(randomFileName))
+         {
+            
+            randomFileName = Path.ChangeExtension(
+               Path.Combine(
+                  Properties.Settings.Default.PhisicalPhotoPath,
+                  NameRandomGenerator()),
+               "jpg");
+         }
+
+         return randomFileName;
+      }
+
+      public static String SaveImage(Bitmap image)
+      {
+         return SaveImage(image, GenerateName());
+      }
+
+      public static string SaveImage(Bitmap image, String fileName)
+      {
+         if (!Directory.Exists(Properties.Settings.Default.PhisicalPhotoPath))
+         {
+            Directory.CreateDirectory(Properties.Settings.Default.PhisicalPhotoPath);
+         }
+         image.Save(fileName, ImageFormat.Jpeg);
+         return fileName;
+      }
+   }
+}
---
+++ trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Service/PhotoAlbumService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetMarche.PhotoAlbum.Service
+{
+   public class PhotoAlbumService : IPhotoAlbumService
+   {
+      #region IPhotoAlbumService Members
+
+      public DotNetMarche.PhotoAlbum.Model.PhotoAlbum CreatePhotoAlbum(DotNetMarche.PhotoAlbum.Model.PhotoAlbum album)
+      {
+         throw new NotImplementedException();
+      }
+
+      public bool AddPhotoToAlbum(string fileName, Guid albumId)
+      {
+         throw new NotImplementedException();
+      }
+
+      #endregion
+   }
+}
