167e871f87a344b06374ff1b11b6b7ffffa9b39d
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2009-03-11 18:46:15 +0000

    added menu for the photoalbum

--- trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Ui.AspNet/Main.Master.designer.cs
+++ trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Ui.AspNet/Main.Master.designer.cs
@@ -32,6 +32,24 @@ namespace DotNetMarche.PhotoAlbum.Ui.AspNet {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
+        /// SiteMapPath1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
+        
+        /// <summary>
         /// LoginName1 control.
         /// </summary>
         /// <remarks>
@@ -39,6 +57,15 @@ namespace DotNetMarche.PhotoAlbum.Ui.AspNet {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LoginName LoginName1;
+        
+        /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
