e2c0b00759dfd938091641d3d526e3b6920efd3a
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2009-04-02 16:55:45 +0000

    Added asp.net mvc support for photoalbum application.

--- trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Ui.AspNet/Global.asax.cs
+++ trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Ui.AspNet/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,11 +15,27 @@ namespace DotNetMarche.PhotoAlbum.Ui.AspNet
 
       protected void Application_Start(object sender, EventArgs e)
       {
+        RegisterRoutes();
+      }
+
+      public static void RegisterRoutes()
+      {
          using (RouteTable.Routes.GetWriteLock())
          {
             RouteTable.Routes.Add(new Route("Photo/{photoid}.axd", new MyPhotoRouter()));
             RouteTable.Routes.Add(new Route("Avatar.axd", new MyAvatarRouter()));
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
+            RouteTable.Routes.MapRoute(
+               "Default",
+               // Route name
+               "{controller}/{action}/{id}",
+               // URL with parameters
+               new { controller = "Home", action = "Index", id = "" }
+               // Parameter defaults
+             );
          }
+        
       }
 
       protected void Session_Start(object sender, EventArgs e)
