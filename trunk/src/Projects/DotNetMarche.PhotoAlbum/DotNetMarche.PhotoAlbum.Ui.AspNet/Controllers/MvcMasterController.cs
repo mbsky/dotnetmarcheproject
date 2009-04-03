62970d738af77137e189e1977c48584eabec8d74
ricci.gianmaria <ricci.gianmaria@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2009-04-03 17:25:42 +0000

    Enabled first master page for asp.net mvc site version.

---
+++ trunk/src/Projects/DotNetMarche.PhotoAlbum/DotNetMarche.PhotoAlbum.Ui.AspNet/Controllers/MvcMasterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using System.IO;
+
+namespace DotNetMarche.PhotoAlbum.Ui.AspNet.Controllers
+{
+   public class MvcMasterController : Controller
+   {
+      private XElement _menuData;
+
+      /// <summary>
+      /// Key is text, and value is the url pointing to.
+      /// </summary>
+      public XElement MenuData
+      {
+         get { return _menuData; }
+      }
+
+      public MvcMasterController()
+      {
+         XDocument doc = XDocument.Load(Path.Combine(Global.PhysicalPath, "WebMvc.sitemap"));
+         XNamespace ns =  XNamespace.Get("http://schemas.microsoft.com/AspNet/SiteMap-File-1.0");
+         _menuData = doc.Root.Element(ns +"siteMapNode");
+         ViewData["Menu"] = _menuData;
+      }
+   }
+}
