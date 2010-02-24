ca28dcf3ed98e51bcc01d3d2185a993a19ec6c8f
ricci.gianmaria@gmail.com <ricci.gianmaria@gmail.com@c7c6ac1c-8670-11dd-a5c9-f1483cedc3eb>
2010-02-24 17:40:31 +0000

    better support for Icomparer

--- trunk/src/Common/Test/DotNetMarche.Common.Test/Infrastructure/Entities/AnEntity.cs
+++ trunk/src/Common/Test/DotNetMarche.Common.Test/Infrastructure/Entities/AnEntity.cs
@@ -12,19 +12,36 @@ namespace DotNetMarche.Common.Test.Infrastructure.Entities
 		public String Name { get; set; }
 		public Int32 Value { get; set; }
 
+		public Int32 fieldValue;
+
 		public static AnEntity Create(Int32 id, String name, Int32 value)
 		{
-			return new AnEntity() {Id = id, Name = name, Value = value};
+			return new AnEntity() { Id = id, Name = name, Value = value, fieldValue = value};
+		}
+
+		public static AnEntity Create(AnEntity original)
+		{
+			return new AnEntity() { Id = original.Id, Name = original.Name, Value = original.Value, fieldValue = original.fieldValue };
 		}
 
 		public static AnEntity CreateSome()
 		{
-			return new AnEntity() { Name = Generator.RandomStringUnique(10), Value = 99 };
+			return new AnEntity() { Name = Generator.RandomStringUnique(10), Value = 99, fieldValue =99 };
 		}
 
 		public static AnEntity CreateWithIdOnly(Int32 id)
 		{
 			return new AnEntity() { Id = id};
 		}
+	}
+
+	internal class AnotherEntity
+	{
+		private Int32 Id { get; set; }
+		public String Name { get; set; }
+		public AnEntity Entity { get; set; }
+
+		public AnEntity fieldEntity;
+
 	}
 }
--- trunk/src/Common/Test/DotNetMarche.Common.Test/TestHelpers/ObjectComparerTest.cs
+++ trunk/src/Common/Test/DotNetMarche.Common.Test/TestHelpers/ObjectComparerTest.cs
@@ -34,6 +34,42 @@ namespace DotNetMarche.Common.Test.TestHelpers
             Assert.That(entity1, DotNetMarche.TestHelpers.SyntaxHelpers.Is.Not.ObjectEqual(entity2));
 		}
 
+		[Test]
+		public void TestBasicCompareDoesNotCompareBackingField()
+		{
+			AnEntity entity1 = AnEntity.Create(10, "test1", 110);
+			AnEntity entity2 = AnEntity.Create(10, "test2", 110);
+			var res = ObjectComparer.FindDifferencies(entity1, entity2);
+			Assert.That(res.Count, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void TestBasicCompareWithField()
+		{
+			AnEntity entity1 = AnEntity.CreateSome();
+			AnEntity entity2 = AnEntity.Create(entity1);
+			entity2.fieldValue = entity2.fieldValue + 1;
+			Assert.That(entity1, DotNetMarche.TestHelpers.SyntaxHelpers.Is.Not.ObjectEqual(entity2));
+		}
+
+		[Test]
+		public void VerifyDoubleNull()
+		{
+			AnEntity entity1 = AnEntity.Create(10, null, 100);
+			AnEntity entity2 = AnEntity.Create(10, null, 100);
+			Assert.That(ObjectComparer.AreEqual(entity1, entity2));
+		}
+
+		[Test]
+		public void CompositeProperty()
+		{
+			AnotherEntity e1 = new AnotherEntity() {Name = "2" , Entity = new AnEntity() {Name = "Test1"}};
+			AnotherEntity e2 = new AnotherEntity() {Name = "2", Entity = new AnEntity() { Name = "Test2" } };
+			var res = ObjectComparer.FindDifferencies(e1, e2);
+			Assert.That(res[0], Text.Contains("Entity.Name"));
+		}
+
+
 		/// <summary>
 		/// this is not a real test, is used only to see the log that 
 		/// a failing assertion does.
